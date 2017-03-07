cce304b3e43f1dc82d74fbb52fa2c63acb58c844
Shomi <somibenz@yahoo.com>
2017-03-07 14:33:59 -0500

    Add files via upload

---
+++ Mass and Weight/Mass and Weight/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mass_and_Weight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                decimal weight = decimal.Parse(textBox.Text) * 9.8M;
+                label.Content = weight;
+                if (weight > 1000)
+                {
+                    MessageBox.Show("It's too heavy");
+                }
+                if (weight < 10)
+                {
+                    MessageBox.Show("It's too light");
+                }
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Please enter a valid input");
+            }
+        }
+    }
+}
